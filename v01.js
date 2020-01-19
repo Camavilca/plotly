5e0b48b45fe6a56acc8851d4cc3702a8637ec42b
camavilca <tesis.tecsup2020@gmail.com>
2020-01-19 09:41:31 -0600

    fic

--- v01.js
+++ v01.js
@@ -7,25 +7,59 @@ const PLOTLYKEY = "hDxj3cXjfb22oXpM2Mmg";
 const plotly = plotlyServer(PLOTLYUSER, PLOTLYKEY);
 
 var trace1 = {
-  x: ["1 %", "2 %", "3 %", "4 %"],
-  y: [10, 15, 13, 17],
+  x: [1, 3, 5, 7],
+  y: ["orlando", "orlando camavilca", "chavez", "manito"],
   type: "bar",
   orientation: "h",
   display: "none",
-  marker: { color: ["#B26223", "#0C4B61", "#11AB93", "#61A40C"] }
+  marker: { color: ["#B26223", "#0C4B61", "#11AB93", "#61A40C"] },
+  text: ["uno", "dos", "tres", "cuatro"],
+  textposition: "left"
 };
 
-var figure = { data: [trace1] };
+var figure = {
+  data: [trace1],
+  layout: {
+    title: "nuevo grafico",
+    font: {
+      family: "Courier New, monospace",
+      size: 10,
+      color: "#7f7f7f"
+    },
+    xaxis: {
+      title: "titlexaxis",
+      titlefont: {
+        size: 10
+      },
+      tickfont: {
+        size: 10
+      },
+      fixedrange: true,
+      range: [0, 10],
+      automargin: true
+    },
+    yaxis: {
+      title: "men",
+      titlefont: {
+        size: 10
+      },
+      tickfont: {
+        size: 10
+      },
+      automargin: true
+    }
+  }
+};
 
 var imgOpts = {
   format: "png",
-  width: 1000,
+  width: 500,
   height: 500
 };
 
 plotly.getImage(figure, imgOpts, function(error, imageStream) {
   if (error) return console.log(error);
 
-  var fileStream = fs.createWriteStream("2.png");
+  var fileStream = fs.createWriteStream("3.png");
   imageStream.pipe(fileStream);
 });
